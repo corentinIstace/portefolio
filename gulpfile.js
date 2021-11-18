728e6eecfd00e14b8690fc18336abdc78e501b68
corentinIstace <corentinistace1@gmail.com>
2021-11-18 14:26:00 +0100

    create gulp file

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+const autoprefixer = require ('autoprefixer');
+const cssnano = require ('cssnano');
+const gulp = require ('gulp');
+const concat = require ('gulp-concat');
+const postcss = require ('gulp-postcss');
+const replace = require ('gulp-replace');
+const sass = require ('gulp-sass')(require('node-sass'));
+const sourcemaps = require ('gulp-sourcemaps');
+const uglify = require ('gulp-uglify');
+// const imagemin = import('gulp-imagemin');
+
+const files = {
+    scssPath: 'scss/**/*.scss',
+    JsPath: 'js/**/*.js',
+    imgPath: 'img/*.{jpg, png}',
+}
+
+function scssTask(){
+    return src(files.scssPath)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('gulp')
+    );
+}
+
+function jstask(){
+    return src (files.JsPath)
+        .pipe(concat('concat.js'))
+        .pipe(uglify())
+        .pipe(dest('gulp'));
+}
+
+// function imgTask(){
+//     return gulp.src(files.imgPath)
+//         .pipe(imagemin([
+//             imagemin.mozjpeg({quality: 75, progressive: true}),
+//             imagemin.optipng({optimizationLevel: 5}),
+//         ]))
+//         .pipe(gulp.dest('gulp/img'));
+// }
+
+const rString = new Date().getTime();
+function cacheBustTask(){
+    return src(['index.html'])
+        .pipe(replace(/cb=\d+/g, 'rs=' + rString))
+        .pipe(dest('.'))
+}
+
+function watchTask(){
+    watch([files.scssPath, files.JsPath, files.imgPath],
+    parallel(scssTask, jstask));
+
+    // watch([files.imgPath], 
+    // series(imgTask));
+}
+
+exports.default = series(
+    parallel(scssTask, jstask),
+    // imgTask,
+    cacheBustTask,
+    watchTask
+);
