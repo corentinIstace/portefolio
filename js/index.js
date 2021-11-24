f87fa80b31aff266cc7085a75517fa57333ed4e8
corentinIstace <corentinistace1@gmail.com>
2021-11-24 16:55:41 +0100

    try to make function for folder hover in project cards

--- js/index.js
+++ js/index.js
@@ -9,4 +9,23 @@ function navbar(){
         console.log("ok");
 };
 
+// let oldIcon = document.getElementsByClassName("fas fa-folder");
+// let newIcon = document.getElementsByClassName("fas fa-folder-open");
+
+// let link = document.getElementsByClassName("project-link");
+// link.addEventListener("mouseover", mouseOver);
+// link.addEventListener("mouseout", mouseOut);
+
+// function mouseOver() {
+//   link.innerHTML += newIcon;
+// }
+// function mouseOut() {
+//   link.style.background += oldIcon;
+// }
+// document.getElementsByClassName("project-link").addEventListener("mouseover", function() {
+//     document.getElementById("project-link").innerHTML = `<i class="fas fa-folder-open"></i>`;
+//   });
+
 navbar();
+mouseOver();
+mouseOut();
