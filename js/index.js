d743ba56792f559efcb129ea579e68a69aa2fdcd
corentinIstace <corentinistace1@gmail.com>
2021-11-26 16:54:37 +0100

    comment all functions

--- js/index.js
+++ js/index.js
@@ -1,31 +1,31 @@
-function navbar(){
-    let menu = document.getElementById("navbar");
-        if (menu.style.display === "block") {
-            menu.style.display = "none";
-        } else {
-            menu.style.display = "block";
-            // menu.style.background = "rgba(79,91,102,0.5)";
-        }
-        console.log("ok");
-};
+// // function navbar(){
+// //     let menu = document.getElementById("navbar");
+// //         if (menu.style.display === "block") {
+// //             menu.style.display = "none";
+// //         } else {
+// //             menu.style.display = "block";
+// //             // menu.style.background = "rgba(79,91,102,0.5)";
+// //         }
+// //         console.log("ok");
+// // };
 
-// let oldIcon = document.getElementsByClassName("fas fa-folder");
-// let newIcon = document.getElementsByClassName("fas fa-folder-open");
+// let link = document.querySelectorAll(".project-link");
+// let oldIcon = `<ion-icon name="folder-outline"></ion-icon>`;
+// let newIcon = `<ion-icon name="folder-open-outline"></ion-icon>`;
 
-// let link = document.getElementsByClassName("project-link");
-// link.addEventListener("mouseover", mouseOver);
-// link.addEventListener("mouseout", mouseOut);
+// function iconsHover(){
+//     link.innerHTML = oldIcon;
 
-// function mouseOver() {
-//   link.innerHTML += newIcon;
-// }
-// function mouseOut() {
-//   link.style.background += oldIcon;
-// }
-// document.getElementsByClassName("project-link").addEventListener("mouseover", function() {
-//     document.getElementById("project-link").innerHTML = `<i class="fas fa-folder-open"></i>`;
-//   });
+//     // for (let i=0; i < link.length; i++){
+//     //     link[i].innerHTML = oldIcon;
+//     // };
 
-navbar();
-mouseOver();
-mouseOut();
+//     for (let i=0; i < link.length; i++){
+//         link[i].addEventListener("mouseover",function(){
+//             link.innerHTML = newIcon;
+//         });
+//     };
+// };
+
+// iconsHover();
+// // navbar();
